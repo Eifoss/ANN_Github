818fab499a45a3c892a6734ef4b4d0de3079dceb
Ida Stoustrup <Ida.Stoustrup@gmail.com>
2018-03-30 13:57:18 +0200

    ANN_Ida has been generalized to n layers with n nodes and seems to work!

--- ANN_Ida.py
+++ ANN_Ida.py
@@ -74,20 +74,22 @@ syn = [0] * len(layersize)
 for i in range(0, len(layersize)-1):
     syn[i] = 2 * np.random.random((layersize[i], layersize[i+1])) - 1
 
-layers = [0] * len(layersize)
-lerrors = [0] * len(layersize)
-ldeltas = [0] * len(layersize)
+NLayers = len(layersize)
+
+layers = [0] * NLayers
+lerrors = [0] * NLayers
+ldeltas = [0] * NLayers
 
 for j in range(60000):
 
     # Feed forward through layers 0, 1, and 2
     layers[0] = X
-    for i in range(1, len(layersize)):
+    for i in range(1, NLayers):
         layers[i] = nonlin(np.dot(layers[i-1], syn[i-1]))
 
     # how much did we miss the target value?
-    lerrors[len(lerrors)] = y - layers[len(layers)]
-    for i in range(len(layersize), 1, -1):
+    lerrors[-1] = y - layers[-1]
+    for i in range(NLayers - 1, 0, -1):
         # in what direction is the target value?
         # were we really sure? if so, don't change too much.
         ldeltas[i] = lerrors[i] * nonlin(layers[i], deriv=True)
@@ -98,11 +100,10 @@ for j in range(60000):
         # # in what direction is the target l1?
         # # were we really sure? if so, don't change too much.
         # ldeltas[i-1] = lerrors[i-1] * nonlin(layers[i-1], deriv=True)
+        syn[i-1] += layers[i-1].T.dot(ldeltas[i])
 
     if j % 10000 == 0:
-        print("The error has been decreased to:" + str(np.mean(np.abs(l2_error))))
+        print("The error has been decreased to:" + str(np.mean(np.abs(lerrors[-1]))))
 
-    syn1 += l1.T.dot(l2_delta)
-    syn0 += l0.T.dot(l1_delta)
 
-print(l2)
+print(layers[-1])
--- ANN_copytut.py
+++ ANN_copytut.py
@@ -24,7 +24,7 @@ np.random.seed(1)
 syn0 = 2 * np.random.random((3, 4)) - 1
 syn1 = 2 * np.random.random((4, 1)) - 1
 
-for j in xrange(60000):
+for j in range(60000):
 
     # Feed forward through layers 0, 1, and 2
     l0 = X
@@ -35,8 +35,7 @@ for j in xrange(60000):
     l2_error = y - l2
 
     if (j % 10000) == 0:
-        print
-        "Error:" + str(np.mean(np.abs(l2_error)))
+        print("Error:" + str(np.mean(np.abs(l2_error))))
 
     # in what direction is the target value?
     # were we really sure? if so, don't change too much.
@@ -51,3 +50,5 @@ for j in xrange(60000):
 
     syn1 += l1.T.dot(l2_delta)
     syn0 += l0.T.dot(l1_delta)
+
+print(l2)
